da668a7f82cc2ef67d59dccc39113abd6fd34356
Jacob Walton <waltojac@mail.gvsu.edu>
2018-10-08 14:46:48 -0400

    Start of History and New.

---
+++ history.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+  <style>
+    tr:nth-child(odd) {
+        background-color: wheat;
+    }
+	tr:nth-child(1) {
+		background-color: black;
+		color: white;
+	}
+    tr:nth-child(even) {
+        background-color: palegreen;
+    }
+  </style>
+</head>
+<body>
+<h1>MoviePlus Rental</h1>
+
+<?php
+require_once '.secret.php';
+
+$db = new mysqli('cis.gvsu.edu', // hostname of db server
+    $mysqluser, // your userid
+    $mysqlpassword, // your password
+    $mydbname);
+
+
+$custId = urldecode($_GET['id']);
+$custName = urldecode($_GET['name']);
+
+printf('<h3>History for customer %s</h3>', $custName);
+
+
+?>
+</body>
+</html>
