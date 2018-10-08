11cd54f5cea31b9d9c4d8c5057e61ec100c4b208
Jake Walton <waltojac@mail.gvsu.edu>
2018-10-08 11:54:03 -0400

    Work on adding link to show customers.

--- home.php
+++ home.php
@@ -60,8 +60,8 @@ LAKER;
     $country = $db->query($countryStr);
     $countryRow = $country->fetch_assoc();
 
-    printf('<tr><td>%s</td><td>%s %s</td><td>%s, %s</td><td>%s</td></tr>',
-        $row['store_id'], $storeRow['first_name'], $storeRow['last_name'], $addressRow['address'], $cityRow['city'], $countryRow['country']);
+    printf('<tr><td><a href="showCustomers.php?storeId=%s">%s</a></td><td>%s %s</td><td>%s, %s</td><td>%s</td></tr>',
+	$row['store_id'], $row['store_id'], $storeRow['first_name'], $storeRow['last_name'], $addressRow['address'], $cityRow['city'], $countryRow['country']);
 }
 printf('</table>');
 ?>
