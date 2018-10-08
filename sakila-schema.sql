dd7a41ea43a24d5099bb034f797edc4a3a1dae69
Jake Walton <waltojac@mail.gvsu.edu>
2018-10-08 11:17:21 -0400

    Fixed db bug.

--- sakila-schema.sql
+++ sakila-schema.sql
@@ -16,9 +16,9 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-DROP SCHEMA IF EXISTS sakila;
-CREATE SCHEMA sakila;
-USE sakila;
+DROP SCHEMA IF EXISTS waltojac;
+ CREATE SCHEMA waltojac;
+ USE waltojac;
 
 --
 -- Table structure for table `actor`
@@ -178,7 +178,7 @@ CREATE TABLE film_text (
   description TEXT,
   PRIMARY KEY  (film_id),
   FULLTEXT KEY idx_title_description (title,description)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Triggers for loading film_text from film
@@ -422,10 +422,10 @@ a.first_name,
 a.last_name,
 GROUP_CONCAT(DISTINCT CONCAT(c.name, ': ',
 		(SELECT GROUP_CONCAT(f.title ORDER BY f.title SEPARATOR ', ')
-                    FROM sakila.film f
-                    INNER JOIN sakila.film_category fc
+                    FROM waltojac.film f
+                    INNER JOIN waltojac.film_category fc
                       ON f.film_id = fc.film_id
-                    INNER JOIN sakila.film_actor fa
+                    INNER JOIN waltojac.film_actor fa
                       ON f.film_id = fa.film_id
                     WHERE fc.category_id = c.category_id
                     AND fa.actor_id = a.actor_id
@@ -433,12 +433,12 @@ GROUP_CONCAT(DISTINCT CONCAT(c.name, ': ',
              )
              ORDER BY c.name SEPARATOR '; ')
 AS film_info
-FROM sakila.actor a
-LEFT JOIN sakila.film_actor fa
+FROM waltojac.actor a
+LEFT JOIN waltojac.film_actor fa
   ON a.actor_id = fa.actor_id
-LEFT JOIN sakila.film_category fc
+LEFT JOIN waltojac.film_category fc
   ON fa.film_id = fc.film_id
-LEFT JOIN sakila.category c
+LEFT JOIN waltojac.category c
   ON fc.category_id = c.category_id
 GROUP BY a.actor_id, a.first_name, a.last_name;
 
