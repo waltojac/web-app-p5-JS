f3a7bec6f70f05121dab98a69ea85790dda2a022
Jacob Walton <waltojac@mail.gvsu.edu>
2018-10-08 20:52:05 -0400

    Test

--- new.php
+++ new.php
@@ -45,7 +45,7 @@ if (isset($_GET['doSearch'])) {
         printf('<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td>');
     }
 } else {
-    printf('<tr><td>Nothing to Display.</td></tr>');
+    printf('<tr>Nothing to Display.</tr>');
 }
 
 printf('</table>');
