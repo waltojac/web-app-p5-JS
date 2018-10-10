9a42cc6f2139e5b6f298e903087a91c7b7820b46
Jacob Walton <waltojac@mail.gvsu.edu>
2018-10-09 20:50:39 -0400

    dev

---
+++ rent.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+<h1>MoviePlus Rental</h1>
+
+<?php
+    printf('<p>Hello World.</p>');
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+</body>
+</html>
