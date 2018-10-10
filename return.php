951c01042e174d63105410715ab0dc47d11cb8a1
Jacob Walton <waltojac@mail.gvsu.edu>
2018-10-10 14:21:57 -0400

    dev

--- return.php
+++ return.php
@@ -6,13 +6,31 @@
 <h1>MoviePlus Rental</h1>
 
 <?php
-require_once '.secret.php';
+    require_once '.secret.php';
 
-$db = new mysqli('cis.gvsu.edu', // hostname of db server
-    $mysqluser, // your userid
-    $mysqlpassword, // your password
-    $mydbname);
+    $db = new mysqli('cis.gvsu.edu', // hostname of db server
+        $mysqluser, // your userid
+        $mysqlpassword, // your password
+        $mydbname);
+        
+    $date = date('Y-m-d H:i:s');
+    $title = urldecode($_GET['title']);
+    $rDate = urldecode($_GET['rDate']);
 
+    $updateStr = <<<LAKER
+    UPDATE inventory
+    SET return_date = $date
+    WHERE title = $title
+    AND rental_date = $rDate
+LAKER;
+
+    if ($db->query($updateStr) === true){
+        printf('<p>Movie Checked-in Successfully.</p>');
+        echo $str;
+    }
+    else {
+        echo "Error: " . $str. "<br>" . $db->error;
+    }
 
 ?>
 
