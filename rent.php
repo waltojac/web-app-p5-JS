949ce5c5efd138d736a4a5dc3a6903562c2b090d
Jacob Walton <waltojac@mail.gvsu.edu>
2018-10-09 21:53:28 -0400

    dev

--- rent.php
+++ rent.php
@@ -8,16 +8,10 @@
 <?php
     session_start();
     $ti = urldecode($_GET['title']);
+    $id = urldecode($_GET['id']);
     printf('<table><tr><td>Customer</tb><td>%s</td></tr>', $_SESSION['cName']);
     printf('<tr><td>Movie Title</td><td>%s</td></tr>', $ti);
-
-
-
-
-
-
-
-
+    printf('<tr><td>Inventory ID</td><td>%s</td></tr>', $ti);
 
 
 
